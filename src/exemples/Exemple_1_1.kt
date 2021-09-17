ff3f9238604d75f6cdc0273434dd1c369951a967
Cherry708 <aluser8241@ieselcaminas.org>
2021-09-17 20:24:21 +0200

    Segundo commit, primero fallido por error en la configuracion del usuario de git

---
+++ src/exemples/Exemple_1_1.kt
@@ -0,0 +1,12 @@
+package exemples
+
+import java.io.File
+
+fun main(args: Array<String>) {
+    val f = File(".") // "." es el root, en este caso Tema1, por lo tanto, veremos lo que contiene
+    println("Llista de fitxers i directoris del directori actual")
+    println("---------------------------------------------------")
+    for (e in f.list())
+        println(e);
+}
+
