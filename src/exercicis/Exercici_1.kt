23b9bb56b6e8c3da73e5139ada013b227ec5e05d
Cherry708 <aluser8241@ieselcaminas.org>
2021-09-22 22:31:36 +0200

    Excepciones capturadas, pruebas del ejercicio base superadas.

--- src/exercicis/Exercici_1.kt
+++ src/exercicis/Exercici_1.kt
@@ -28,42 +28,55 @@ fun main() {
      */
     while (userInput != -1) {
 
-        println("Introduce un numero (-1 para acabar): ")
-        userInput = readLine()!!.toInt()
-        userSelection = userInput - userInputOffset
+        try {
+            println("Introduce un numero (-1 para acabar): ")
+                userInput = readLine()!!.toInt()
+                userSelection = userInput - userInputOffset
 
-        //En esta condicion verificamos que el input sea 0, no debemos usar userSelection
-        //Si retrocedo en root peta, prueba try catch
+            //En esta condicion verificamos que el input sea 0, no debemos usar userSelection
+            //Si retrocedo en root peta, prueba try catch
 
-        if (userInput == 0 && f.parentFile.exists()) {
+            if (userInput == 0 && f.parentFile.exists()) {
 
-            //f.parentFile
-            f = f.parentFile
-            listarDirectorios(f)
+                //f.parentFile
+                f = f.parentFile
+                listarDirectorios(f)
 
-        } else if (userInput == -1) {
-            println("Saliendo...")
+            } else if (userInput == -1) {
+                println("Saliendo...")
 
-        } else if (userSelection in 0..f.listFiles().size && f.listFiles().sorted().get(userSelection).isDirectory
-            && isReadable(f,userSelection)) {
+            } else if (userSelection in 0..f.listFiles().size && isDirectory(f, userSelection) && isReadable(f, userSelection)
+            ) {
 
-            f = f.listFiles().sorted().get(userSelection)
-            listarDirectorios(f)
+                f = f.listFiles().sorted().get(userSelection)
+                listarDirectorios(f)
 
-        } else if (!isReadable(f,userSelection)){
+            } else if (!isReadable(f, userSelection)) {
 
-            println("No tienes acceso a este directorio.")
+                println("No tienes acceso a este directorio.")
 
-        } else if (!f.listFiles().sorted().get(userSelection).isDirectory) {
+            } else if (!f.listFiles().sorted().get(userSelection).isDirectory) {
 
-            println("El destino no es un directorio.")
+                println("El destino no es un directorio.")
 
-        } else if (!(userSelection in 1..f.listFiles().size - 1)) {
+            }
+        } catch (e: AccessDeniedException){
+            println("No tienes acceso a este directorio B.")
+        } catch (e: NumberFormatException){
+            println("Error de entrada.")
+            println("Debes introducir el índice del destino.")
+        } catch (e: ArrayIndexOutOfBoundsException){
+            println("El destino esta fuera de rango. Comprueba el indice.")
+        } catch (e: NullPointerException){
+            /*
+            Windows:
+            En ocasiones el usuario requiere permisos del administrador de Windows
+            y resulta en esta excepción. La excepción que normalmente lanza el objeto
+            al no haber parent también puede ser lanzada por este motivo.
+             */
+            println("No hay parent o puedes no tener permisos de administrador para ese destino(Windows).")
+            println("Estás en: "+f.canonicalPath)
 
-            println("El destino esta fuera de rango o no existe. Comprueba el indice.")
-            println()
-
-            listarDirectorios(f)
         }
 
         //If end is not a directory, f:FILE will not be reassigned so 'if' in this branch will happen
@@ -82,6 +95,10 @@ fun isReadable(f: File, userSelection:Int): Boolean{
     return f.listFiles().sorted().get(userSelection).canRead()
 }
 
+fun isDirectory(f: File, userSelection:Int): Boolean{
+    return f.listFiles().sorted().get(userSelection).isDirectory
+}
+
 fun listarDirectorios(f: File) {
     val s = "Lista de ficheros y directorios del directorio " + f.getCanonicalPath()
     println(s)
@@ -91,7 +108,7 @@ fun listarDirectorios(f: File) {
     println("0.- Introduce 0 para volver al parent")
     for (file in f.listFiles().sorted()) {
         if (file.isFile())
-            println("$fileCounter.- " + file.getName() + "\t " + file.length())
+            println("$fileCounter.- " + file.getName() + "\t " + "<Fichero>"+"[Tamaño: "+file.length()+"]")
         if (file.isDirectory())
             println("$fileCounter.- " + file.getName() + "\t <Directorio>")
         fileCounter++
